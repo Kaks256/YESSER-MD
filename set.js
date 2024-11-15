332d7eb0069706f6aecfcc652da52e41630ecbe4
Kaks256 <167288747+Kaks256@users.noreply.github.com>
2024-11-15 15:31:24 +0300

    Update set.js

--- set.js
+++ set.js
@@ -7,11 +7,11 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'zokk',
+module.exports = { session: process.env.SESSION_ID || 'Bmwmd$JjdWNfVX',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "yessertech",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "255621995482",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    OWNER_NAME: process.env.OWNER_NAME || "google",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "256743391566",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
     BOT : process.env.BOT_NAME || 'Yesser Md ',
     URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/37882de26f9ffc60043ef.jpg',
